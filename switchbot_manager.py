8533481805f31a23ff0cb528441482d19e3b1c33
kiri <kiri0803@gmail.com>
2021-03-28 19:02:19 +0900

    結果のJSONファイル書き出しに対応

--- switchbot_manager.py
+++ switchbot_manager.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import requests
 import pprint
 import pathlib
+import json
 
 # .envを読み込んで環境変数を設定
 load_dotenv()
@@ -12,11 +13,17 @@ load_dotenv()
 API_KEY = os.getenv("SWITCHBOT_APIKEY")
 
 # 共通
+output_dir = pathlib.Path("output")
 base_url = "https://api.switch-bot.com"
 headers = {
     "Authorization" : API_KEY
 }
 
+def save_json(json_dic, filename):
+    output_dir.mkdir(exist_ok=True)
+    filepath = output_dir / pathlib.Path(filename)
+    with open(filepath, "w") as f:
+        json.dump(json_dic, f, ensure_ascii=False, indent=4)
 
 def get_devices():
     api_url = "/v1.0/devices"
@@ -27,7 +34,8 @@ def get_devices():
 
 def main():
     res_dic = get_devices()
-    pprint.pprint(res_dic, indent=2)
+    pprint.pprint(res_dic, indent=4)
+    save_json(res_dic, "devicelist.json")
     print("完了")
 
 if __name__ == "__main__":
